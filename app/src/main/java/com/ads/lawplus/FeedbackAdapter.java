35dd70e9170c64cca9c610fc216fa03974ae36d3
saduni-udeshika <saduniudeshika0@gmail.com>
2022-11-06 19:54:21 +0530

    View feedback

--- app/src/main/java/com/ads/lawplus/FeedbackAdapter.java
+++ app/src/main/java/com/ads/lawplus/FeedbackAdapter.java
@@ -1,7 +1,38 @@
 package com.ads.lawplus;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class FeedbackAdapter {
+import java.util.ArrayList;
 
+public class FeedbackAdapter extends RecyclerView.Adapter<FeedbackAdapter.ViewHolder>{
+    Context context;
+    ArrayList<Feedback> list;
+
+    public FeedbackAdapter(Context context, ArrayList<Feedback> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public FbAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.feedbackentry, parent, false);
+        return new ViewHolder();
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FbAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
