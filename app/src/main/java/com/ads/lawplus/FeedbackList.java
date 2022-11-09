ea8edafeac7aa6e063d31cbf62f9be955e7f09ad
saduni-udeshika <saduniudeshika0@gmail.com>
2022-11-09 19:01:10 +0530

    Create feedback floating action button

--- app/src/main/java/com/ads/lawplus/FeedbackList.java
+++ app/src/main/java/com/ads/lawplus/FeedbackList.java
@@ -4,15 +4,17 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
 import java.util.ArrayList;
 
 public class FeedbackList extends AppCompatActivity {
@@ -21,6 +23,7 @@ public class FeedbackList extends AppCompatActivity {
     DatabaseReference database;
     FeedbackAdapter feedbackAdapter;
     ArrayList<Feedbacks> feedbackList;
+    FloatingActionButton feedback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +52,17 @@ public class FeedbackList extends AppCompatActivity {
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
+            }
+        });
+
+        //Add feedback button navigation
+        feedback = findViewById(R.id.floatingActionButton);
+
+        feedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(FeedbackList.this, AddFeedback.class));
             }
         });
 
